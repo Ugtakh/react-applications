c4c15b56ec816e96fab9fbb3c1ef8be9de52681c
Ugtakhbayar Surakhbayar <ugtakhbayars@gmail.com>
2023-01-06 18:24:18 +0800

    Add bootstrap an react-strap

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,15 +1,23 @@
 // import css from "./App.module.css";
-import "./App.css";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+// import "./App.scss";
+// import Home from "./page/home";
+// import Detail from "./page/detail";
+import "./custome.scss";
+import { Button } from "react-bootstrap";
 
 function App() {
+  // return <Detail />;
   return (
     <div>
-      <Header />
-      <Content />
-      <Footer />
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </div>
   );
 }
---
+++ first-app/src/page/home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header";
+import Content from "../../components/Content";
+import Footer from "../../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
